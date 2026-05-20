0e30bcd83fa816d7ecb322a617bcbf433b09b98e
Kipleting Edwin <kipletingedwin4@gmail.com>
2026-05-20 09:28:05 +0100

    commit

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/Button"
-import { Menu } from "lucide-react"
+import { Menu, X } from "lucide-react"
+import { useState } from "react"
 
 
 const navLinks = [
@@ -10,6 +11,10 @@ const navLinks = [
 ]
 
 export const Navbar = () => {
+
+  const[isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+
   return (
     <header className='fixed top-0 left-0 right-0 bg-transparent py-5'>
        <nav className='container mx-auto px-6 flex items-center justify-between'>
@@ -40,12 +45,32 @@ export const Navbar = () => {
         </div>
 
         {/* Mobile Menu Button */}
-        <button className="md:hidden p-2 text-foreground">
-          <Menu />
+        <button className="md:hidden p-2 text-foreground"
+        onClick={() => setIsMobileMenuOpen((prev => !prev))}
+        >
+          { isMobileMenuOpen ? <X /> : <Menu size={24} />}
         </button>
-
-
        </nav>
+
+       {/* Mobile Menu */}
+       { isMobileMenuOpen && (<div className="md:hidden glass-strong">
+        <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+        {
+              navLinks.map((link, index)=> (
+                <a 
+                href={link.href} 
+                key={index}
+                className="text-lg text-muted-foreground hover:text-foreground py-2"
+                >{link.label}</a>
+              ))
+            }
+            <Button>Contact Me</Button>
+
+        </div>
+       </div>)
+       }
+
+
     </header>
   )
 }
